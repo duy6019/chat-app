ab99ea0f9da94a1b9277fa1d85404b8765029b6f
sacarozo <duy.mta.1412@gmail.com>
2019-09-20 22:05:23 +0700

    Sua? loi~

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,3 @@
 module.exports.homeController = require('./homeController');
-module.exports.authController = require('./authController'); 
+module.exports.authController = require('./authController');
+ 
